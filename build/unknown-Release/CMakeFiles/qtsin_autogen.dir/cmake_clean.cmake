2d0fe6f8aedb6816f5cd6a28b56697d530264cab
sheng yu <sy13212871325@163.com>
2025-09-22 10:38:52 +0800

    首次提交

---
+++ build/unknown-Release/CMakeFiles/qtsin_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qtsin_autogen"
+  "qtsin_autogen/mocs_compilation.cpp"
+  "qtsin_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/qtsin_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
